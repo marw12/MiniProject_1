341dcb16b6bc8d6b84f798b043757693c69547ff
marw12 <marwan.khan@mail.mcgill.ca>
2020-10-06 22:19:04 -0400

    setup

---
+++ code.py
@@ -0,0 +1,26 @@
+# Importing the libraries
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+
+
+
+#Data Preprocessing
+
+# Importing the dataset
+dataset_1 = pd.read_csv('2020_US_weekly_symptoms_dataset.csv')  #Search Trends dataset
+dataset_2 = pd.read_csv('aggregated_cc_by.csv', dtype={"test_units": "object"})  #hospitalization cases dataset
+dataset_2 = dataset_2.iloc[78164:90022]  #Loading rows for USA only and rows that don't have all missing values
+
+#Cleaning the datasets
+
+dataset_1 = dataset_1.dropna(axis='columns', how='all')  #removes columns with all NaN values
+dataset_1 = dataset_1.dropna(axis='rows', how='all')  #removes rows with all NaN values
+
+dataset_2 = dataset_2.dropna(axis='columns', how='all')  #removes columns with all NaN values
+dataset_2 = dataset_2.dropna(axis='rows', how='all')  #removes rows with all NaN values
+
+#Match time resolution
+
+pd.set_option('display.max_columns', None)
+print(dataset_2)
