0759dd6ee2d89cb91866357254f1f41abf273af7
marw12 <marwan.khan@mail.mcgill.ca>
2020-10-07 16:23:29 -0400

    imputation

--- code.py
+++ code.py
@@ -3,6 +3,8 @@ import numpy as np
 import pandas as pd
 
 
+### TASK 1
+
 #Data Preprocessing
 
 # Importing the dataset
@@ -29,4 +31,72 @@ dataset_2 = dataset_2.groupby(['open_covid_region_code', pd.Grouper(key='date', 
 
 #merging on multiple columns
 dataset_3 = pd.merge(dataset_1, dataset_2[['date', 'open_covid_region_code', 'hospitalized_new']], on=['date', "open_covid_region_code"])
+
+### TASK 3
+
+#convert dataframe into numpy array for calculations
+X = dataset_3.iloc[:, 5:-1].values  #get all the columns except the last one
+y = dataset_3.iloc[:, -1].values  #get just the last column
+
+# One Hot Encoding the "Geography" column
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OneHotEncoder
+ct = ColumnTransformer(transformers=[('encoder', OneHotEncoder(), [0])], remainder='passthrough')
+X = np.array(ct.fit_transform(X))
+print(X)
+
+# Taking care of missing data
+from sklearn.impute import SimpleImputer
+imputer = SimpleImputer(missing_values = np.nan, strategy = 'mean')
+imputer.fit(X[:, :])
+X[:, :] = imputer.transform(X[:, :])
+
+# Splitting the dataset into the Training set and Test set
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y,train_size = 0.8, test_size = 0.2, random_state = 0)
+
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
+
+#training the KNN model on the training data
+from sklearn.neighbors import KNeighborsRegressor
+knn = KNeighborsRegressor(n_neighbors = 5, metric = 'minkowski', p = 2)
+knn.fit(X_train, y_train)
+
+#predicting
+y_pred = knn.predict(X_test)
+print(y_pred)
+
+#accuracy
+from sklearn.model_selection import cross_val_score
+scores = cross_val_score(knn, X_train, y_train, cv=5)
+print(scores)
+print("Accuracy: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
